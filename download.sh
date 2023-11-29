daccf19e1425ce1e2d749c7920ed0b5da9f0af99
Nick <zhong.ren@hotmail.com>
2023-11-29 10:21:28 -0500

    Replace to use post-processed NCBI protein coding gene file

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 # Download the bgee gene expression calls
 wget --timestamping --directory-prefix data/input/ https://www.bgee.org/ftp/current/download/calls/expr_calls/Homo_sapiens_expr_simple.tsv.gz
 
-# Download the Human gene_info.gz file of Entrez Genes
-wget --timestamping --directory-prefix data/input/ ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz
+# Download NCBI Entrez protein-coding Genes
+wget --timestamping --directory-prefix data/input/ https://raw.githubusercontent.com/nickzren/human-gene/main/data/output/protein_coding_gene.csv
--- process.py
+++ process.py
@@ -1,29 +1,27 @@
 import pandas as pd
+import csv
 import gzip
 
-GENE_INFO_PATH = 'data/input/Homo_sapiens.gene_info.gz'
+GENE_INFO_PATH = 'data/input/protein_coding_gene.csv'
 EXPRESSION_INFO_PATH = 'data/input/Homo_sapiens_expr_simple.tsv.gz'
-NODE_OUTPUT_PATH = 'data/output/node_Anatomy.csv'
-EDGE_OUTPUT_PATH = 'data/output/edge_Gene_expressedIn_Anatomy.csv'
+NODE_OUTPUT_PATH = 'data/output/csv/node_Anatomy.csv'
+EDGE_OUTPUT_PATH = 'data/output/csv/edge_Gene_expressedIn_Anatomy.csv'
 NODE_COLUMNS = {'Anatomical entity ID': 'identifier', 'Anatomical entity name': 'name'}
 EDGE_COLUMNS = {'NCBI Gene ID': 'source_id', 'Anatomical entity ID': 'target_id', 'is_gold_quality': 'is_gold_quality'}
 
-def read_gene_info(file_path):
-    """Read gene information, filter for protein-coding genes, and extract Ensembl IDs."""
-    try:
-        with gzip.open(file_path, 'rt') as file:
-            gene_df = pd.read_csv(file, delimiter='\t')
-            protein_coding_genes = gene_df[gene_df['type_of_gene'] == 'protein-coding'].copy()
-            protein_coding_genes.loc[:, 'Ensembl'] = protein_coding_genes['dbXrefs'].str.extract('Ensembl:(ENSG[0-9]+)')
-            return protein_coding_genes
-    except IOError as e:
-        print(f"Error reading file {file_path}: {e}")
-        return pd.DataFrame()
+def load_gene_data_from_csv(file_path):
+    ensembl_to_geneid = {}
+    symbol_to_geneid = {}
 
-def create_mappings(gene_df):
-    """Create mappings from Ensembl ID and Symbol to GeneID."""
-    ensembl_to_geneid = pd.Series(gene_df['GeneID'].values, index=gene_df['Ensembl']).to_dict()
-    symbol_to_geneid = pd.Series(gene_df['GeneID'].values, index=gene_df['Symbol']).to_dict()
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            gene_id = row['GeneID']
+            gene_symbol = row['Symbol']
+            ensembl = row['Ensembl']
+            ensembl_to_geneid[ensembl] = gene_id
+            symbol_to_geneid[gene_symbol] = gene_id
+
     return ensembl_to_geneid, symbol_to_geneid
 
 def read_and_filter_expression_data(file_path):
@@ -48,8 +46,7 @@ def save_to_csv(df, file_path, rename_columns):
     df[list(rename_columns.keys())].drop_duplicates().rename(columns=rename_columns).to_csv(file_path, index=False)
 
 def main():
-    gene_df = read_gene_info(GENE_INFO_PATH)
-    ensembl_to_geneid, symbol_to_geneid = create_mappings(gene_df)
+    ensembl_to_geneid, symbol_to_geneid = load_gene_data_from_csv(GENE_INFO_PATH)
     expression_df = read_and_filter_expression_data(EXPRESSION_INFO_PATH)
     expression_df = map_gene_ids(expression_df, ensembl_to_geneid, symbol_to_geneid)
 
